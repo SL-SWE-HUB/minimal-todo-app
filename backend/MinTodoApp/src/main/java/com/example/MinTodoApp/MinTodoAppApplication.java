37aeac139e684154552f0f42e50c344d6861538b
Luna <luna@Lunas-MacBook-Air.local>
2025-02-19 16:07:46 -0600

    Spring Initializr files

---
+++ backend/MinTodoApp/src/main/java/com/example/MinTodoApp/MinTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MinTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinTodoAppApplication.class, args);
+	}
+
+}
---
+++ backend/MinTodoApp/src/test/java/com/example/MinTodoApp/MinTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MinTodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
